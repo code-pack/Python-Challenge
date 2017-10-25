c9549137377d44caef8c5a45c6dc2fe01e12891a
David W Jones <davidandlee.jones@gmail.com>
2017-10-25 16:50:48 -0500

    documented draft still needs file output added

--- pybank/main.py
+++ pybank/main.py
@@ -1,8 +1,18 @@
+# -*- coding: UTF-8 -*-
+""" Python Homework  PyBank
+    Python Script that will input monthly revenue data formated in a csv
+    file containing "Date", "Revenue". Output to screen and file lists 
+    total number of months, total revenue, average change month over month, and
+    the month and amount for the greatest increase and greatest decrease.
+    
+     David W. Jones           """
 
+
+# Dependencies
 import os
 import csv
 
-# request file names for input and output, declair the subfolder for holding the data
+# request file names for input and output, declair the subfolder for holding the input data
 # If i had time I would add an IF statement to prompt again if file not found
 
 input_file = input(" Please enter the name of the data file (with csv ext): ")
@@ -10,7 +20,7 @@ csvpath = os.path.join("raw_data", input_file)
 output_file = input("Please enter the name of the Text file to hold the Financial Review (with txt ext): ")
 
 
-# Read the csv and convert it into a list of dictionaries
+# Declaration of variables that will be utilized during the looping and output process
 
 month_count=0
 total_revenue=0
@@ -24,8 +34,9 @@ current_month_rev=0
 prior_month_rev=0
 delta=0
 
-
-
+#looping process to gather the output information that fills the variables 
+#skips the first month for calculating the monthly change (exclude) keeps a 
+#running tab of months and revenue.  
 
 with open(csvpath) as revenue_data:
     reader = csv.DictReader(revenue_data)
@@ -33,13 +44,13 @@ with open(csvpath) as revenue_data:
         prior_month_rev=current_month_rev 
         if prior_month_rev==0:
             exclude=int(row["Revenue"])
-            exclude_date=row["Date"]
         total_revenue=total_revenue+ int(row["Revenue"])
         month_count = month_count + 1
-        
+        # calculates the change in current month revenue and keeps total     
         current_month_rev=int(row["Revenue"])
         delta=current_month_rev-prior_month_rev
         avg_delta=avg_delta+delta
+        # flags the largest loss or largest gain
         if delta > lrg_gain:
             lrg_gain=delta
             gain_date=row["Date"]
@@ -47,29 +58,17 @@ with open(csvpath) as revenue_data:
             lrg_loss=delta
             loss_date=row["Date"]
         
-
+#calculates the average change over the period
 avg_delta=avg_delta-exclude
 delta_percentage=avg_delta/(month_count-1)
-
-
-print(total_revenue)
-print(month_count)
-print(lrg_gain)
-print(lrg_loss)
-print(loss_date)
-print(gain_date)
-print(avg_delta)
-print(delta_percentage)
-print(exclude)
-print(exclude_date)
-
-
-
-
-#Financial Analysis
-#----------------------------
-#Total Months: 25
-#Total Revenue: $1241412
-#Average Revenue Change: $216825
-#Greatest Increase in Revenue: Sep-16 ($815531)
-#Greatest Decrease in Revenue: Aug-12 ($-652794)   
+#outputs the results
+print("  ")
+print("--------------------------------------------------------------------")
+print(" Financial Analysis")
+print("--------------------------------------------------------------------")
+print("Total number of months in period: " + str(month_count))
+print("Total Revenue in period: $ " + str(total_revenue))
+print("Average monthly change in Revenue : $" + str(delta_percentage))
+print("Greatest monthly increase in Revenue : " + str(gain_date) + "   $ " + str(lrg_gain))
+print("Largest monthly decrease in Revenue : " + str(loss_date) + "   $ " + str(lrg_loss))
+print("--------------------------------------------------------------------")
